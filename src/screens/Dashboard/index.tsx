e14051c517be45bd88492dcc13982e088a8e63be
KainanGV <kainangabriel2019@gmail.com>
2023-04-24 11:15:49 -0300

    feat: cards transaction

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,26 @@
 import React from "react"
-import { Container, Title } from "./styles"
+import { Container, Header, Icon, Photo, User, UserGreeting, UserInfo, UserName, UserWrapper } from "./styles"
+import { HighlightCard } from "../../components/HighlightCard"
 
 export function Dashboard() {
   return (
       <Container>
-        <Title>
-          Dashboard
-        </Title>
+        <Header> 
+          <UserWrapper>
+            <UserInfo>
+              <Photo source={{uri: "https://avatars.githubusercontent.com/u/71570785?v=4"}} />
+              <User>
+                <UserGreeting>Olá</UserGreeting>
+                <UserName>Rodrigo</UserName>
+              </User>
+            </UserInfo>
+
+            <Icon name="power" />
+          </UserWrapper>
+
+        </Header>
+
+        <HighlightCard />
       </Container>
   )
 }
